7f36090f24cf2bdddeb05c4056c32b49c8d17508
Joe DePung and Michael Petter <ionicmobile+joe.depung+michael.petter@asynchrony.com>
2016-06-21 15:50:04 -0500

    change max retry to 20 for conventional backoff handler for reasonable time limit

--- lib/sneakers_handlers/conventional_exponential_backoff_handler.rb
+++ lib/sneakers_handlers/conventional_exponential_backoff_handler.rb
@@ -16,23 +16,22 @@
 #  | Attempt  4:                7.5s |               13.0s |
 #  | Attempt  5:               15.5s |               28.5s |
 #  | Attempt  6:               31.5s |            1m  0.0s |
-#  |                                 |                     |
 #  | Attempt  7:            1m  3.5s |            2m  3.5s |
 #  | Attempt  8:            2m  7.5s |            4m 11.0s |
 #  | Attempt  9:            4m 15.5s |            8m 26.5s |
 #  | Attempt 10:            8m 31.5s |           16m 58.0s |
 #  | Attempt 11:           17m  3.5s |           34m  1.5s |
 #  | Attempt 12:           34m  7.5s |        1h  8m  9.0s |
-#  |                                 |                     |
 #  | Attempt 13:        1h  8m 15.5s |        2h 16m 24.5s |
 #  | Attempt 14:        2h 16m 31.5s |        4h 32m 56.0s |
 #  | Attempt 15:        4h 33m  3.5s |        9h  5m 59.5s |
 #  | Attempt 16:        9h  6m  7.5s |       18h 12m  7.0s |
 #  | Attempt 17:       18h 12m 15.5s |    1d 12h 24m 22.5s |
-#  |                                 |                     |
 #  | Attempt 18:    1d 12h 24m 31.5s |    3d     48m 54.0s |
 #  | Attempt 19:    3d     49m  3.5s |    6d  1h 37m 57.5s |
-#  | Attempt 20:    6d  1h 38m  7.5s |   12d  3h 16m  5.0s |
+#  |---------------------------------+---------------------|
+#  | Attempt 20:    6d  1h 38m  7.5s |   12d  3h 16m  5.0s | <-- default max retries
+#  |---------------------------------+---------------------|
 #  | Attempt 21:   12d  3h 16m 15.5s |   24d  6h 32m 20.5s |
 #  | Attempt 22:   24d  6h 32m 31.5s |   48d 13h  4m 52.0s |
 #  | Attempt 23:   48d 13h  5m  3.5s |   97d  2h  9m 55.5s |
@@ -50,7 +49,7 @@
 #                "x-dead-letter-routing-key" => "my-app.queue_name" }}
 #
 # The following defaults can be overriden:
-#   max_retries: 25     # The number of times it retries before dead-lettering a message.
+#   max_retries: 20     # The number of times it retries before dead-lettering a message.
 #
 # from_queue "my-app.queue_name",
 #   exchange: "my_exchange_name",
@@ -63,7 +62,13 @@
 module SneakersHandlers
   class ConventionalExponentialBackoffHandler < ConfigurableBackoffHandler
     def initialize(channel, queue, options)
-      super(channel, queue, options.merge(delay_strategy: -> (x) { (2**x - 1) / 2.0 }))
+      default_options = { max_retries: 20 }
+
+      forced_options = {
+        delay_strategy: -> (x) { (2**x - 1) / 2.0 }
+      }
+
+      super(channel, queue, default_options.merge(options).merge(forced_options))
     end
   end
 end
